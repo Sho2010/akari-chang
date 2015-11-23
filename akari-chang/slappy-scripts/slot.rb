cc813ae9a645b3793c753e7568122f36ad6194b3
Sho2010 <Sho2010@naitou-no-MacBook-Pro.local>
2015-11-23 11:33:40 +0900

    Implement casino

--- akari-chang/slappy-scripts/slot.rb
+++ akari-chang/slappy-scripts/slot.rb
@@ -3,6 +3,15 @@ students = %w(:ayano: :chitose: :himawari: :rise: :sakurako:)
 goraku = %w(:akari: :chi-nya: :kyoko: :yui:)
 slot_sources = [students, goraku]
 
+def slot_hit?(patterns)
+  # 揃ったカウント返したほうがいいかもな
+  return true if patterns[0,3].uniq.length == 1
+  return true if patterns[3,3].uniq.length == 1
+  return true if patterns[6,3].uniq.length == 1
+  return true if patterns.values_at(0, 4, 8).uniq.length == 1
+  return true if patterns.values_at(2, 4, 6).uniq.length == 1
+end
+
 hear /slot|スロット/ do |event|
   src = slot_sources.sample
   result = []
@@ -10,3 +19,16 @@ hear /slot|スロット/ do |event|
   say result.join(' '), channel: event.channel
   say("わぁい 揃ったよ♪", channel: event.channel) if result.uniq.length == 1
 end
+
+hear /カジノ|casino/ do |event|
+  src = slot_sources.sample
+  result = []
+  9.times{ result << src.sample }
+
+  lines = result.each_slice(3) 
+  say lines.map(&:join).join("\n"), channel: event.channel # join join ﾄｷｨ
+  
+  say("わぁい 揃ったよ♪", channel: event.channel) if slot_hit?(result) 
+  say(":bell::moneybag:フィーバー♪♪:bell::moneybag:", channel: event.channel) if result.uniq.length == 1
+end
+
